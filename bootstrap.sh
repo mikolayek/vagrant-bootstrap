964705c3a7f547e27fb399bb9d7cb3d8cf11a3e3
Paul <mikolayek@gmail.com>
2014-11-24 01:16:47 +0100

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y vim
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java7-installer
